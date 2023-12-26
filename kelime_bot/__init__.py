6a2f91f6c63b872d3bee3b42d3116f6c1d26c0bc
MEYİTZADE <silaatesss.31@gmail.com>
2023-12-26 10:17:46 +0300

    Update __init__.py

--- kelime_bot/__init__.py
+++ kelime_bot/__init__.py
@@ -1,45 +1,23 @@
+from os import getenv
+from kelime_bot.config import Config
+from telethon import TelegramClient
 from time import sleep
-from pyrogram import Client
 import logging
 from dotenv import load_dotenv, set_key, unset_key
-from os import getenv
-
-load_dotenv('config.env')
-
-# THE LOGGING
+ 
+ 
 logging.basicConfig(
     level=logging.INFO,
-    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-logging.getLogger("pyrogram").setLevel(logging.WARNING)
-LOGGER = logging.getLogger(__name__)
-
-
-# Hesap
-API_ID = getenv("API_ID", "2930013")
-API_HASH = getenv("API_HASH", "7cab92dcd979add511b79d693775e17d")
-TOKEN = getenv("TOKEN", "6803669543:AAF3IVN9OQvi0DFESZSX2Nc4W2YGFBO6__c")
-USERNAME = getenv("USERNAME", "MytGrupBot")
-OWNER_ID = getenv("OWNER_ID", "1527476177")
-
-if OWNER_ID and len(OWNER_ID) and OWNER_ID.isdigit():
-    OWNER_ID = int(OWNER_ID)  # type: ignore
-else:
-    OWNER_ID = None  # type: ignore
-
-# BOT CLIENTİ
-bot = Client(
-    ":memory:",
-    API_ID,
-    API_HASH,
-    bot_token=TOKEN,
-    plugins=dict(root="kelime_bot/plugins/"),
-    workers=16
+    format='%(name)s - [%(levelname)s] - %(message)s'
 )
-
-
-# Oyun Verileri
-oyun = {}  # type: ignore
-
-
-# rating
-rating = {}  # type: ignore
+LOGGER = logging.getLogger(__name__)
+ 
+ 
+ 
+api_id = Config.API_ID
+api_hash = Config.API_HASH
+bot_token = Config.BOT_TOKEN
+ 
+tag = TelegramClient('tag', api_id, api_hash).start(bot_token=bot_token)
+ 
+ 

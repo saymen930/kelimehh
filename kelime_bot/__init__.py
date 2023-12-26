6b7efe42311c9a4d667627cca0e63ae0205b9961
MEYİTZADE <silaatesss.31@gmail.com>
2023-12-26 08:43:04 +0300

    Add files via upload

---
+++ kelime_bot/__init__.py
@@ -0,0 +1,51 @@
+from time import sleep
+from pyrogram import Client
+import logging
+
+
+# THE LOGGING
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+logging.getLogger("pyrogram").setLevel(logging.WARNING)
+LOGGER = logging.getLogger(__name__)
+
+
+
+
+
+# Hesap
+API_ID = "15781409"
+API_HASH = "0499f7f54f3fbb56b43acb6edf2d9696"
+TOKEN = "5444327778:AAEL9SrrRTKPGkTKIACKSLJ55qlnNNCIGo8" 
+USERNAME = "5444327778"
+
+
+
+
+# BOT CLIENTİ
+bot = Client(
+    ":memory:",
+    API_ID,
+    API_HASH,
+    bot_token=TOKEN,
+    plugins=dict(root="kelime_bot/plugins/"),
+    workers=16
+    )
+
+
+# Oyun Verileri
+oyun = {}
+
+
+# rating
+rating = {}
+
+
+
+
+
+# !!!!!!!!!!!!!! DEĞİŞTİR KESİNLİKLE !!!!!!!!!!!!!!!!
+#      SAHİBİN USER ID'Sİ
+OWNER_ID = 974246367
+

5ed872119c7db9eba9190fbc179731995f5353ef
Saymen930 <115408305+saymen930@users.noreply.github.com>
2024-10-05 00:56:20 +0400

    Update config.py

--- kelime_bot/config.py
+++ kelime_bot/config.py
@@ -2,11 +2,11 @@ import os
  
 class Config:
  
-   API_ID = int(os.getenv("API_ID", "2930013"))
-   API_HASH = os.getenv("API_HASH", "7cab92dcd979add511b79d693775e17d")
-   BOT_TOKEN = os.getenv("BOT_TOKEN", "6803669543:AAF3IVN9OQvi0DFESZSX2Nc4W2YGFBO6__c")
-   BOT_USERNAME = os.environ.get("BOT_USERNAME", "MytGrupBot")
-   OWNER_ID = int(os.environ.get("OWNER_ID","1527476177"))
-   OWNER_NAME = os.environ.get("OWNER_NAME", "Meyit47")
-   SUPPORT = os.environ.get("SUPPORT", "S1F1RB1RCHAT")
-   BOT_NAME = os.environ.get("BOT_NAME", "MytGrupBot")
+   API_ID = int(os.getenv("API_ID", "16501912"))
+   API_HASH = os.getenv("API_HASH", "34138f0596a914d229f8bc1ab7794bc6")
+   BOT_TOKEN = os.getenv("BOT_TOKEN", "6999080991:AAFm-Sad9EBJuGYA_y8BWLKNyHSMtBmgLeA")
+   BOT_USERNAME = os.environ.get("BOT_USERNAME", "EsilaChatBot")
+   OWNER_ID = int(os.environ.get("OWNER_ID","6153472412"))
+   OWNER_NAME = os.environ.get("OWNER_NAME", "debubluman")
+   SUPPORT = os.environ.get("SUPPORT", "sorundestekk")
+   BOT_NAME = os.environ.get("BOT_NAME", "Esila Sohbet")
